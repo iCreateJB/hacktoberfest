292e7f8ca90e459347d72569be5767ced092b122
Jonathan Ballard <jonathan.ballard@imagineeasy.com>
2016-10-07 07:31:04 -0400

    Add date filtering and authentication against bad user names.

--- main.go
+++ main.go
@@ -32,7 +32,10 @@ var ghClient *github.Client
 
 func commonHeaders(h http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    w.Header().Set("Access-Control-Allow-Origin","*")
+    w.Header().Set("X-Frame-Options","deny")
+    w.Header().Set("X-Content-Type-Options","no-sniff")
 		h.ServeHTTP(w, r)
 	}
 }
@@ -62,12 +65,14 @@ func PullRequestHandler(w http.ResponseWriter, r *http.Request) {
     w.Write(resp)
     return
   }
-  events, _, err := ghClient.Activity.ListEventsPerformedByUser(ghUser,true,nil);
+  opt    := &github.ListOptions{PerPage: 100}
+  fmt.Printf("[b] %v\n",ghUser)
+  events, _, err := ghClient.Activity.ListEventsPerformedByUser(ghUser,true,opt);
   if  err != nil { fmt.Println(err) }
   for _,event := range events {
-    if *event.Type == "PullRequestEvent" {
+    repo_name    := *event.Repo.Name
+    if *event.Type == "PullRequestEvent" && inTimeSpan(*event.CreatedAt) {
       pull_request := event.Payload().(*github.PullRequestEvent).PullRequest
-      repo_name    := *event.Repo.Name
       submission   := Submission{ID: *pull_request.Number, Title: *pull_request.Title, Login: *pull_request.User.Login, URL: *pull_request.HTMLURL, State: *pull_request.State,RepoName: repo_name, CreatedAt: *pull_request.CreatedAt}
       s.PullRequests = append(s.PullRequests,submission)
     }
@@ -76,6 +81,12 @@ func PullRequestHandler(w http.ResponseWriter, r *http.Request) {
   resp,_ := json.Marshal(s)
   w.WriteHeader(http.StatusOK)
   w.Write(resp)
+}
+
+func inTimeSpan(check time.Time) bool {
+  start,_ := time.Parse(time.RFC822, "01 Oct 16 00:00 UTC")
+  end,_   := time.Parse(time.RFC822, "31 Oct 16 23:59 UTC")
+  return check.After(start) && check.Before(end)
 }
 
 func portNumber() string {
